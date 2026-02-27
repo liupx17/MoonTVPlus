26a16a35f8c09fb264f9ff498401054cf612429b
liupx <liupx17@gmail.com>
2026-02-27 15:28:39 +0800

    Update route.ts

--- src/app/api/danmaku/search/route.ts
+++ src/app/api/danmaku/search/route.ts
@@ -3,7 +3,8 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import { getConfig } from '@/lib/config';
 
-export const runtime = 'nodejs';
+// export const runtime = 'nodejs';
+export const runtime = 'edge';
 
 export async function GET(request: NextRequest) {
   try {
